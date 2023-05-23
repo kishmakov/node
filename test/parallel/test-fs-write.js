512e672b66f2ee0a19026e8217f307cb83771f98
Shelley Vohr <shelley.vohr@gmail.com>
2023-05-23 18:21:17 +0200

    fix: account for createExternalizableString V8 global
    Introduced in https://chromium-review.googlesource.com/c/v8/v8/+/4531903.
    
    This patch can be removed when Node.js upgrades to a version of V8 with the above
    CL - they'll need to make the same change.
    
    Patch-Dir: src/electron/patches/node
    Patch-Filename: fix_account_for_createexternalizablestring_v8_global.patch

--- test/parallel/test-fs-write.js
+++ test/parallel/test-fs-write.js
@@ -38,7 +38,7 @@ const constants = fs.constants;
 const { externalizeString, isOneByteString } = global;
 
 // Account for extra globals exposed by --expose_externalize_string.
-common.allowGlobals(externalizeString, isOneByteString, global.x);
+common.allowGlobals(createExternalizableString, externalizeString, isOneByteString, global.x);
 
 {
   const expected = 'ümlaut sechzig';  // Must be a unique string.
