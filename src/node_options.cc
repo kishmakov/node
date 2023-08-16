25a06674fd04bbc61e1c6907a6b8435b0fb697ca
Shelley Vohr <shelley.vohr@gmail.com>
2023-08-16 19:15:29 +0200

    fix: assert module in the renderer process
    When creating a Node.js Environment, embedders have the option to disable Node.js'
    default overriding of Error.prepareStackTrace. However, the assert module depends on
    a WeakMap that is populated with the error stacktraces in the overridden function.
    
    This adds handling to fall back to the default implementation if Error.prepareStackTrace
    if the override has been disabled.
    
    This will be upstreamed.
    
    Patch-Dir: src/electron/patches/node
    Patch-Filename: fix_assert_module_in_the_renderer_process.patch

--- src/node_options.cc
+++ src/node_options.cc
@@ -1405,14 +1405,16 @@ void GetEmbedderOptions(const FunctionCallbackInfo<Value>& args) {
   }
   Isolate* isolate = args.GetIsolate();
 
-  constexpr size_t kOptionsSize = 4;
+  constexpr size_t kOptionsSize = 5;
   std::array<Local<Name>, kOptionsSize> names = {
+      FIXED_ONE_BYTE_STRING(env->isolate(), "hasPrepareStackTraceCallback"),
       FIXED_ONE_BYTE_STRING(env->isolate(), "shouldNotRegisterESMLoader"),
       FIXED_ONE_BYTE_STRING(env->isolate(), "noGlobalSearchPaths"),
       FIXED_ONE_BYTE_STRING(env->isolate(), "noBrowserGlobals"),
       FIXED_ONE_BYTE_STRING(env->isolate(), "hasEmbedderPreload")};
 
   std::array<Local<Value>, kOptionsSize> values = {
+      Boolean::New(isolate, env->prepare_stack_trace_callback().IsEmpty()),
       Boolean::New(isolate, env->should_not_register_esm_loader()),
       Boolean::New(isolate, env->no_global_search_paths()),
       Boolean::New(isolate, env->no_browser_globals()),
