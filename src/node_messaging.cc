b19b1b909de491f7faab1a540ed8bafb2ee63da7
deepak1556 <hop2deep@gmail.com>
2023-12-26 02:10:42 +0900

    fix: capture embedder exceptions before entering V8
    Upstrem bug: https://github.com/nodejs/node-v8/issues/274
    
    The patch only addresses the callsites that triggered failing DCHECKS
    in the nodejs test suite. Need to be followed-up with upstream
    on the broader change as there maybe other callsites.
    
    Patch-Dir: src/electron/patches/node
    Patch-Filename: fix_capture_embedder_exceptions_before_entering_v8.patch

--- src/node_messaging.cc
+++ src/node_messaging.cc
@@ -907,7 +907,7 @@ Maybe<bool> MessagePort::PostMessage(Environment* env,
                                      const TransferList& transfer_v) {
   Isolate* isolate = env->isolate();
   Local<Object> obj = object(isolate);
-
+  TryCatchScope try_catch(env);
   std::shared_ptr<Message> msg = std::make_shared<Message>();
 
   // Per spec, we need to both check if transfer list has the source port, and
@@ -915,6 +915,10 @@ Maybe<bool> MessagePort::PostMessage(Environment* env,
 
   Maybe<bool> serialization_maybe =
       msg->Serialize(env, context, message_v, transfer_v, obj);
+  if (try_catch.HasCaught() &&
+      !try_catch.HasTerminated()) {
+    try_catch.ReThrow();
+  }
   if (data_ == nullptr) {
     return serialization_maybe;
   }
